c9297890c291b59806cd2f8090cd10d5441e3bcb
Uo <102210447+Uo1428@users.noreply.github.com>
2023-10-02 20:12:48 +0500

    commonjs to modulejs

--- Assets/Config/config.mjs
+++ Assets/Config/config.mjs
@@ -2,26 +2,25 @@ import dotenv from "dotenv"
 dotenv.config()
 
 export default {
-  TOKEN: process.env.TOKEN || "",
-  MONGO_DB: process.env.MONGO_DB || "",
-  CLIENT_ID: process.env.CLIENT_ID || "",
-  Prefix: "z",
-  OWNERS: ["922120042651451423"],
-  SUPPORT_SERVER: "",
+  TOKEN: process.env.TOKEN || "", // Bot token 
+  MONGO_DB: process.env.MONGO_DB || "", // MongoDB (not required)
+  CLIENT_ID: process.env.CLIENT_ID || "", // Bot ID
+  Prefix: "z", // Bot Prefix 
+  OWNERS: ["922120042651451423"], 
+  SUPPORT_SERVER: "", // soon
   Channels: {
-    CommandLogs: "1041589448523128874",
-    CommandErrorLogs: "1041589448523128874"
+    CommandLogs: "1041589448523128874", // channel id where you want to Command Logs
+    CommandErrorLogs: "1041589448523128874" // Channel Id
   },
 
   Activity: {
-    name: "yoo",
-    type: 3
+    name: "yoo", // status
+    type: 3 // eg 1, 2, 3, 4
   },
-  Status: "dnd",
+  Status: "dnd", // dnd, online, idle, invisivble 
   
-  Settings: {
-    CommandLogs: true,
+  Settings: { 
+    CommandLogs: true, 
     CommandErrorLogs: true,
   }
 }
-
--- src/client.mjs
+++ src/client.mjs
@@ -1,7 +1,9 @@
 import { Client, Collection, GatewayIntentBits as GIB, Partials, Routes, REST, EmbedBuilder } from 'discord.js';
-import fs from 'fs';
+import fs from 'fs/promises';
 import logger from './utils/logger.mjs';
-class client extends Client {
+import { mongoose } from 'mongoose'
+
+class Bot extends Client {
     constructor() {
         super({
             allowedMentions: {
@@ -31,7 +33,15 @@ class client extends Client {
         this.config = config;
         this.embed = embed;
         this.emotes = emotes;
-        this.Embed = () => new EmbedBuilder;
+        this.Embed = (footer = true) => {
+            let embed = new EmbedBuilder();
+            if (!embed.color) embed.setColor(this.embed.color);
+            if (footer) embed.setFooter({
+                text: this.embed.footertext,
+                iconURL: this.embed.footericon
+            });
+            return embed;
+        };
 
         ["events", "aliases", "buttons", "commands", "cooldowns", "categories", "slashCommands"].forEach(i => this[i] = new Collection());
 
@@ -39,80 +49,101 @@ class client extends Client {
         await this.login(config.TOKEN);
 
         if (!config.CLIENT_ID) this.config.CLIENT_ID = this.application.id;
+
+        if(config.MONGO_DB){       
+            mongoose.set('strictQuery', true);
+
+        mongoose.connect(config.MONGO_DB, {
+            autoIndex: false,
+            maxPoolSize: 10,
+            serverSelectionTimeoutMS: 5000,
+            socketTimeoutMS: 45000,
+            family: 4,
+        })
+        .then(() => logger("Connected to MongoDB".bold))
+        .catch((err) => console.error("MongoDB ❌\n", err));
+    }
     }
 
     async loadEvents() {
-        fs.readdirSync(`./src/events`).filter((file) => file.endsWith('.mjs')).forEach(async (event) => {
-            const { default: clientEvent } = await import(`./events/${event}`);
-            if (clientEvent.ignore || !clientEvent.name || !clientEvent.run) return;
+        await fs.readdir(`./src/events`).then((i) => {
+            i.filter((file) => file.endsWith('.mjs')).forEach(async (event) => {
+                const { default: clientEvent } = await import(`./events/${event}`);
+                if (clientEvent.ignore || !clientEvent.name || !clientEvent.run) return;
 
-            this.events.set(clientEvent.name, clientEvent);
-            if (clientEvent.customEvent) return clientEvent.run(this);
+                this.events.set(clientEvent.name, clientEvent);
+                if (clientEvent.customEvent) return clientEvent.run(this);
 
-            if (clientEvent.runOnce) this.once(clientEvent.name, (...args) => clientEvent.run(this, ...args));
-            else this.on(clientEvent.name, (...args) => clientEvent.run(this, ...args));
-        });
+                if (clientEvent.runOnce) this.once(clientEvent.name, (...args) => clientEvent.run(this, ...args));
+                else this.on(clientEvent.name, (...args) => clientEvent.run(this, ...args));
+            });
+        })
     }
 
     async loadCommands() {
 
         const TOKEN = this.config.TOKEN;
         const CLIENT_ID = this.config.CLIENT_ID;
-        const rest = new REST({ version: '10' }).setToken(TOKEN);
+        const rest = new REST({ version: '9' }).setToken(TOKEN);
 
         let slashCommands = [];
 
-        fs.readdirSync("./Commands/Slash").forEach(async dir => {
-            const files = fs.readdirSync(`./Commands/Slash/${dir}/`).filter(file => file.endsWith('.mjs'));
-            for (const file of files) {
-                const { default: slashCommand } = await import("../Commands/Slash/" + dir + "/" + file);
-                if (slashCommand.ignore) return;
-
-                slashCommands.push(slashCommand.data);
-
-                if (slashCommand.data.name) this.slashCommands.set(slashCommand.data.name, slashCommand);
-                else throw `Command Error: ${slashCommand.name || file.split('.mjs')[0] || "Missing Name"} - ${this.user.username}`
-            }
-
-            await rest.put(
-                Routes.applicationCommands(CLIENT_ID), {
-                body: slashCommands
-            }).then(() => logger(`Loaded Slash Commands for ${this.user.username}`.bold))
-
+        await fs.readdir("./Commands/Slash").then(async i => {
+            i.forEach(async dir => {
+                await fs.readdir(`./Commands/Slash/${dir}/`).then(async files => {
+                    files.filter(file => file.endsWith('.mjs'));
+                    for (const file of files) {
+                        const { default: slashCommand } = await import("../Commands/Slash/" + dir + "/" + file);
+                        if (slashCommand.ignore) return;
+                        slashCommands.push(slashCommand.data);
+                        if (slashCommand.data.name) this.slashCommands.set(slashCommand.data.name, slashCommand);
+                        else throw `Command Error: ${slashCommand.name || file.split('.mjs')[0] || "Missing Name"} - ${this.user.username}`
+                    }
+                })
+            })
         });
 
         this.fatchedCommands = await this.application.commands.fetch();
 
         // ======== Message Commands
 
-        fs.readdirSync("./Commands/Prefix").forEach(async dir => {
-            const files = fs.readdirSync(`./Commands/Prefix/${dir}/`).filter(file => file.endsWith('.mjs'));
-            for (const file of files) {
-                const { default: prefixCommand } = await import("../Commands/Prefix/" + dir + "/" + file);
-                if (prefixCommand.ignore) return;
-                if (prefixCommand.name) {
-                    let i;
-                    if (Array.isArray(prefixCommand.name)) {
-                        prefixCommand.name.forEach(name => {
-                            const clonedCommand = JSON.parse(JSON.stringify(prefixCommand));
-                            this.commands.set(name, clonedCommand);
-                            clonedCommand.name = name;
-                            clonedCommand.description = clonedCommand.description.replace("{commandName}", name);
-                            clonedCommand.run = prefixCommand.run;
-                        })
-                    } else this.commands.set(prefixCommand.name, prefixCommand);
-                } else throw `Prefix Command Error: ${prefixCommand.name || file.split('.mjs')[0] || "Missing Name"} - ${this.user.username}`
-                if (prefixCommand.aliases && Array.isArray(prefixCommand.aliases)) prefixCommand.aliases.forEach(messageCommandAlias => {
+        await fs.readdir("./Commands/Prefix").then(i => {
+            i.forEach(async dir => {
+                await fs.readdir(`./Commands/Prefix/${dir}/`).then(async files => {
+                    files.filter(file => file.endsWith('.mjs'));
+                    for (const file of files) {
+                        const { default: prefixCommand } = await import("../Commands/Prefix/" + dir + "/" + file);
+                        if (prefixCommand.ignore) return;
+                        if (prefixCommand.name) {
+                            let i;
+                            if (Array.isArray(prefixCommand.name)) {
+                                prefixCommand.name.forEach(name => {
+                                    const clonedCommand = JSON.parse(JSON.stringify(prefixCommand));
+                                    this.commands.set(name, clonedCommand);
+                                    clonedCommand.name = name;
+                                    clonedCommand.description = clonedCommand.description.replace("{commandName}", name);
+                                    clonedCommand.run = prefixCommand.run;
+                                })
+                            } else this.commands.set(prefixCommand.name, prefixCommand);
+                        } else throw `Prefix Command Error: ${prefixCommand.name || file.split('.mjs')[0] || "Missing Name"} - ${this.user.username}`
+                        if (prefixCommand.aliases && Array.isArray(prefixCommand.aliases)) prefixCommand.aliases.forEach(messageCommandAlias => {
 
-                    this.aliases.set(prefixCommand, prefixCommand.name);
-                });
-            }
+                            this.aliases.set(prefixCommand, prefixCommand.name);
+                        });
+                    }
+                })
+            })
 
-            logger(`Loaded Prefix Commands for ${this.user.username}`.bold)
         });
+
+        await rest.put(
+            Routes.applicationCommands(CLIENT_ID), {
+            body: slashCommands
+        }).then(() => logger(`Loaded Slash Commands for ${this.user.username}`.bold));
+        logger(`Loaded Prefix Commands for ${this.user.username}`.bold)
 
     }
 
 }
 
-export default client;
+export default Bot;
